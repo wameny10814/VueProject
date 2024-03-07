b72ce29fa7028e573366cd5b30fef50708a9048a
YuChen <104507463+wameny10814@users.noreply.github.com>
2024-03-07 18:24:41 +0800

    Updates

---
+++ assets/AboutView-4m4BUZKt.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,a as o,w as a,b as e,F as r,f as c,r as _,o as u}from"./index-5lWXgtDH.js";const i={},l=c("h1",null,"This is About page.",-1);function p(d,f,m,b,h,x){const t=_("RouterLink");return u(),n(r,null,[l,o(t,{to:"/"},{default:a(()=>[e("Home")]),_:1}),e(" | "),o(t,{to:"/about"},{default:a(()=>[e("About")]),_:1})],64)}const V=s(i,[["render",p]]);export{V as default};
