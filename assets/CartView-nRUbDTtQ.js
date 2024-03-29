905cf013a893d1a2faa4774a5d79572b81d9bef5
YuChen <104507463+wameny10814@users.noreply.github.com>
2024-03-29 16:18:30 +0800

    Updates

---
+++ assets/CartView-nRUbDTtQ.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-aRQ9i-Q4.js";const o={};function r(s,a,n,p,_,i){return c(),t("h1",null,"This is cart page.")}const l=e(o,[["render",r]]);export{l as default};
--- assets/CartView-swYH8Rtd.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as c}from"./index-10-N6Rut.js";const o={};function r(s,a,n,p,_,i){return c(),t("h1",null,"This is cart page.")}const l=e(o,[["render",r]]);export{l as default};
